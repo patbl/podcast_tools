0dc303efc04383a0c688ec2b0bbb56c6283ab2e9
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2017-10-29 15:03:33 -0700

    Add utility for bulk-downloading audio

---
+++ lib/bulk_download_youtube_audio.rb
@@ -0,0 +1,16 @@
+class BulkDownloadYoutubeAudio
+  attr_reader :urls
+
+  def self.call(urls)
+    new(urls).call
+  end
+
+  def initialize(urls)
+    @urls = urls
+  end
+
+  def call
+    threads = urls.map { |url| Thread.new { DownloadYoutubeAudio.new(url).call } }
+    threads.each(&:join)
+  end
+end
