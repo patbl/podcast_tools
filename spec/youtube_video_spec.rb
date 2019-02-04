0604bba7a0f059b4e8305a346adb53603f50a54e
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2019-02-03 17:55:00 -0800

    YouTube: Extract title and description given URL

---
+++ spec/youtube_video_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "support/vcr_config"
+require_relative "../lib/youtube_video"
+
+RSpec.describe YoutubeVideo do
+  let(:video) { described_class.new("https://www.youtube.com/watch?v=yf9GljlqjI4") }
+
+  describe "#description" do
+    it "returns the description" do
+      VCR.use_cassette("YouTube Video: An Introduction to Normative Uncertainty") do
+        video = described_class.new("https://www.youtube.com/watch?v=yf9GljlqjI4")
+        expect(video.description).to start_with("If you want to have the highest possible impact")
+      end
+    end
+  end
+
+  describe "#title" do
+    it "returns the title" do
+      VCR.use_cassette("YouTube Video: An Introduction to Normative Uncertainty") do
+        expect(video.title).to eq "An Introduction to Normative Uncertainty"
+      end
+    end
+  end
+end
