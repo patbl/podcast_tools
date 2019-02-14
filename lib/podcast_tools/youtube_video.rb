d9bcd539fa276568249b13bd3f6159dd7a8ba88d
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2019-02-13 22:23:41 -0800

    Extract and decode external links

--- lib/podcast_tools/youtube_video.rb
+++ lib/podcast_tools/youtube_video.rb
@@ -14,10 +14,13 @@ module PodcastTools
     def description
       html.at_css("#eow-description").children.map { |child|
         if child.name == "a"
-          href = CGI.parse(child.attributes["href"].value).fetch("q").fetch(0)
+          href = child.attributes["href"].value
+          next child unless href.start_with?(%r{/redirect})
+          uri = URI.parse(href)
           new_node = Nokogiri::XML::Node.new("a", html)
           new_node.content = child.content
-          new_node["href"] = href
+          new_href = CGI.parse(uri.query).fetch("q").fetch(0)
+          new_node["href"] = new_href
           new_node
         else
           child
